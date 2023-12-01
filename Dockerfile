23a9c2112029656523ff0503403f8b7d1ab632ec
Anttestuser <pyth44a@gmail.com>
2023-12-01 14:24:41 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM python:3.10.9-alpine3.16
+
+SHELL ["/bin/bash", "-c"]
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+EXPOSE 8000
+
+RUN pip install --upgrade pip
+
+RUN apt update
+
+RUN useradd -rms /bin/bash foxuser && chmod 777 /opt /run
+
+RUN mkdir /web/static && mkdir /web/media && chown -R foxuser:foxuser /web && chmod 755 /web
+
+COPY --chown=foxuser:foxuser . .
+
+RUN pip install -r requirements.txt
+
+USER foxuser
+
+CMD ["gunicorn", "-b", "0.0.0.0:8000", "web.wsgi.application"]
--- web/settings.py
+++ web/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os.path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e0a_pt=a&_11^_hhzlfs*glxahw@-y0pitngeyim9y+0$-tl0e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOST"),]
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
     }
 }
 
@@ -118,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+MEDIA_ROOT = 'media'
+
 # STATIC_ROOT = '/static'
 # STATIC_ROOT = '/static'
 
