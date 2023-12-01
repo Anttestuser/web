9cf95c4bc35b7bc1bbc1baf7cc7da74cd8b9137a
Anttestuser <pyth44a@gmail.com>
2023-12-01 20:38:23 +0300

    init

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 python manage.py migrate
-
 python manage.py collectstatic --noinput
-
 gunicorn --bind 0.0.0.0:8080 web.wsgi
--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOST"),]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOST", '').split()
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR / 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,11 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-MEDIA_ROOT = 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 
-# STATIC_ROOT = '/static'
-# STATIC_ROOT = '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
