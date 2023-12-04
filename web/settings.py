5d2f9be33405706ff8706d581fdb772c2e985b82
Anttestuser <pyth44a@gmail.com>
2023-12-04 18:12:41 +0300

    init

--- web/settings.py
+++ web/settings.py
@@ -72,16 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'web.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
-#     }
-# }
-#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
