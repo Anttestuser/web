ab17cd6c7046ff8a8e3bd4429a65aa068c43879b
Anttestuser <pyth44a@gmail.com>
2023-12-12 12:49:56 +0300

    media upload

--- hey/models.py
+++ hey/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class UploadFiles(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default='')
     file = models.ImageField(upload_to='uploads_model')
