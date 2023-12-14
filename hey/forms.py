826df0a35b3b5621152430f7baf7e8fa95651c06
Anttestuser <pyth44a@gmail.com>
2023-12-14 12:52:36 +0300

    nginx settings

--- hey/forms.py
+++ hey/forms.py
@@ -6,3 +6,11 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = UploadFiles
         fields = ['name', 'file']
+
+from .models import *
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = UploadFiles
+        fields = ['name', 'file']
